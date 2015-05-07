89a2e8bb29f645e72e013274869443f18d60720e
Dr. Michael Lauer <mickey@vanille-media.de>
2015-05-07 16:10:01 +0200

    factor out the database wrapper – that way we might also support multiple databases in the future!?

--- mops/mops.py
+++ mops/mops.py
@@ -1,3 +1,11 @@
+"""
+MOPS - MongoDB Prototyping Shell
+
+(C) 2015 Dr. Michael 'Mickey' Lauer <mickey@vanille.de>
+
+https://github.com/mickeyl/mops
+"""
+
 import pymongo
 import bson
 import json
@@ -5,46 +13,74 @@ import readline
 import cmd
 import os
 import sys
+import clint
 
+#############################################################################
 class MongoJSONEncoder( json.JSONEncoder ):
-     def default(self, obj):
-         if isinstance(obj, bson.ObjectId):
-             return repr( obj )
-         # Let the base class default method raise the TypeError
-         return json.JSONEncoder.default(self, obj)
 
-class Mops():
+    """
+    Our custom JSON encoder to facilitate encoding some of the mongodb types which are not standard JSON.
+    """
 
-    def mongoDatabaseNames( self ):
+    def default(self, obj):
+        if isinstance(obj, bson.ObjectId):
+            return repr( obj )
+        return json.JSONEncoder.default(self, obj)
+
+#############################################################################
+class DatabaseWrapper():
+
+    """
+    Implementation of the mongodb commands
+    """
+
+    def __init__( self ):
+        self.mc = pymongo.MongoClient()
+
+    def databaseNames( self ):
         return self.mc.database_names()
 
-    def mongoCollectionNames( self, dbname ):
+    def collectionNames( self, dbname ):
         return self.mc[dbname].collection_names()
 
-    def mongoDocuments( self, dbname, collection, limit, query={} ):
+    def queryDocuments( self, dbname, collection, limit, query={} ):
         return list( self.mc[dbname][collection].find( query ).limit( limit ) )
 
-    def mongoDistinct( self, dbname, collection, fieldname ):
+    def distinctValuesForField( self, dbname, collection, fieldname ):
         return self.mc[dbname][collection].distinct( fieldname )
+
+#############################################################################
+class Mops():
+
+    """
+    The Shell.
+    """
 
     def __init__(self):
         self.prompt = "mops> "
-        self.intro  = "Welcome to MOPS – MOngodb Prototype Shell!\n"  ## defaults to None
-        self.mc = pymongo.MongoClient()
+        self.intro = "Welcome to MOPS – MOngodb Prototype Shell!\n"
+        self.outro = "\nThanks for using MOPS – Have a nice day!"
+        self.db = DatabaseWrapper()
+
+    def shutdown( self ):
+        print( self.outro )
 
     def complete( self, text, state ):
+        """
+        Readline completion
+        """
         # print( "%s (%s)" % ( text, state ) )
         if state == 0:
             origline = readline.get_line_buffer()
-            begin = readline.get_begidx()
-            end = readline.get_endidx()
-            being_completed = origline[begin:end]
+            #begin = readline.get_begidx()
+            #end = readline.get_endidx()
+            #being_completed = origline[begin:end]
             components = origline.split( " " )
 
             if len( components ) < 2:
-                allmatches = self.mongoDatabaseNames()
+                allmatches = self.db.databaseNames()
             elif len( components ) == 2:
-                allmatches = self.mongoCollectionNames( components[0] )
+                allmatches = self.db.collectionNames( components[0] )
             else:
                 allmatches = []
 
@@ -53,6 +89,9 @@ class Mops():
         return self.matches[state] if state < len( self.matches ) else None
 
     def run( self ):
+        """
+        Implementation of the command loop
+        """
         print( self.intro )
         readline.set_completer_delims( readline.get_completer_delims().replace( '-', '' ) )
         readline.parse_and_bind( "tab:complete")
@@ -61,14 +100,14 @@ class Mops():
             line = input( self.prompt )
             components = line.strip().split()
             if len( components ) == 0:
-                result = self.mongoDatabaseNames()
+                result = self.db.databaseNames()
             elif len( components ) == 1:
                 dbname = components[0]
-                result = self.mongoCollectionNames( dbname )
+                result = self.db.collectionNames( dbname )
             elif len( components ) == 2:
                 dbname = components[0]
                 collection = components[1]
-                result = self.mongoDocuments( dbname, collection, limit=15 )
+                result = self.db.queryDocuments( dbname, collection, limit=15 )
             elif len( components ) == 3:
                 dbname = components[0]
                 collection = components[1]
@@ -77,20 +116,21 @@ class Mops():
                 if query.startswith( "{" ):
                     try:
                         dictionary = eval( components[2] )
-                        result = self.mongoDocuments( dbname, collection, limit=15, query=dictionary )
+                        result = self.db.queryDocuments( dbname, collection, limit=15, query=dictionary )
                     except Exception:
                         result = repr( sys.exc_info() )
                 else:
-                    result = self.mongoDistinct( dbname, collection, fieldname=query )
+                    result = self.db.distinctValuesForField( dbname, collection, fieldname=query )
             else:
                 result = "?"
-            
+
             print( json.dumps( result, sort_keys=True, indent=2, separators=( ',', ': ' ), cls=MongoJSONEncoder ) )
 
 
+#############################################################################
 if __name__ == '__main__':
     console = Mops()
     try:
         console.run()
     except ( EOFError, KeyboardInterrupt ):
-        print( "\nThanks for using MOPS!" )
+        console.shutdown()
